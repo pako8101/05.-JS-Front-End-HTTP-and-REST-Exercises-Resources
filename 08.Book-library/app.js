da6d9486e8d3f571694b7a2ac8cd21e9e7bd04d6
pako8101 <pako810129@gmail.com>
2024-12-11 13:36:06 +0200

    fetch books edit and delete

--- 08.Book-library/app.js
+++ 08.Book-library/app.js
@@ -1,3 +1,5 @@
+const baseUrl = 'http://localhost:3030/jsonstore/collections/books';
+
 function attachEvents() {
   const loadBooksButton = document.getElementById("loadBooks");
   const bookListElement = document.getElementById("book-list");
@@ -39,11 +41,32 @@ createBookButton.addEventListener('click',(e) => {
 const title = titleInput.value;
 const author = authorInput.value;
 
-const bookElement = createBookItem({title,author})
+fetch(baseUrl, {
+  method: 'POST',
+  headers:{
+'Content-Type': 'application/json'
+  },
+body: JSON.stringify({
+  title:title,
+  author,
+})
+})
+
+.then(res => res.json())
+.then(result => {
+  const bookElement = createBookItem({title,author})
 bookListElement.appendChild(bookElement);
 
 titleInput.value = '';
 authorInput.value = '';
+  
+})
+.catch(err => {
+  console.log(err);
+  
+})
+
+
 
 }
 )
@@ -70,12 +93,32 @@ authorInput.value = '';
 function createBookItem(book){
 const bookTr = document.createElement('tr')
 
-bookTr.innerHTML = `<td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </td>`;
+// bookTr.innerHTML = `<td>${book.title}</td>
+//                 <td>${book.author}</td>
+//                 <td>
+//                     <button>Edit</button>
+//                     <button>Delete</button>
+//                 </td>`;
+
+const titleTd = document.createElement('td');
+titleTd.innerText = book.title;
+const authroTd = document.createElement('td');
+authroTd.textContent = book.author;
+
+
+const editButton = document.createElement('button');
+editButton.textContent = 'Edit';
+const deleteButton = document.createElement('button');
+deleteButton.textContent = 'Delete';
+
+const butonsTd = document.createElement('td');
+
+butonsTd.appendChild(editButton);
+butonsTd.appendChild(deleteButton);
+
+bookTr.appendChild(titleTd);
+bookTr.appendChild(authroTd);
+bookTr.appendChild(butonsTd);
 
                 return bookTr;
 
