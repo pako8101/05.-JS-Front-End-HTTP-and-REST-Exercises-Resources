a7dfa3436f7db106de4b01293c9071491fb49a4e
pako8101 <pako810129@gmail.com>
2024-12-27 09:36:30 +0200

    Messenger fetch

--- 05.Messenger/app.js
+++ 05.Messenger/app.js
@@ -1,5 +1,50 @@
 function attachEvents() {
-    // TODO:
+    
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+
+    const [nameInput,contentInput,sendButton,refreshButton] = document.getElementsByTagName('input');
+    const messagesTextArea = document.getElementById('messages');
+
+    sendButton.addEventListener('click', async () =>{
+        const messageFormat = {
+            author: nameInput.value,
+            content: contentInput.value
+        };
+
+        let isValidMessage = nameInput.value !== ''
+        && contentInput !== '';
+
+if (isValidMessage) {
+      await fetch(baseUrl,{
+            method:'POST',
+            body: JSON.stringify(messageFormat)
+        });
+}
+      
+        nameInput.value = '';
+        contentInput.value = '';
+    });
+
+    refreshButton.addEventListener('click',async () => {
+        
+        const response = await fetch(baseUrl);
+
+        const messages = await response.json();
+
+        const messagesToDisplay = [];
+
+        for (const messageInfo of Object.values(messages)) {
+            
+            messagesToDisplay.push(`${messageInfo.author}: ${messageInfo.content}`);
+
+        }
+ messagesTextArea.textContent = messagesToDisplay.join('\n')
+  
+        
+
+    })
+   
+
 }
 
-attachEvents();
+attachEvents();      console.log(messages);
