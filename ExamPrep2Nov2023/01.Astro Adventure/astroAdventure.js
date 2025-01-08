cc2f06df0e1cdac17b76c04eba82421f6d74afb1
pako8101 <pako810129@gmail.com>
2025-01-08 10:51:58 +0200

    astroAdventure alternative Decision

--- ExamPrep2Nov2023/01.Astro Adventure/astroAdventure.js
+++ ExamPrep2Nov2023/01.Astro Adventure/astroAdventure.js
@@ -11,39 +11,68 @@ function solve(input) {
             oxygen: Number(oxygen),
             energy: Number(energy),
         });
-        
+
 
     }
-while (input.length > 0) {
-    const command = input.shift();
+    while (input.length > 0) {
+        const command = input.shift();
 
-    if (command === 'End') {
-        break;
-    }
-const [action,name,value] = command.split(' - ');
-switch (action) {
-    case 'Explore':
-        const energyNeeded = Number(value);
-        const astronaut = astronauts.find(astronaut => astronauts.name ===name);
-
-        if (astronaut.energy >= value) {
-            console.log();
-            
+        if (command === 'End') {
+            break;
         }
-        
-        break;
-        case 'Refuel':
-        
-        break;
-        case 'Breath':
-        
-        break;
-    default:
-        break;
-}
+        const [action, name, value] = command.split(' - ');
+        switch (action) {
+            case 'Explore':
+                const energyNeeded = Number(value);
+                const astronaut = astronauts.find(astronaut => astronaut.name === name);
 
-}
+                if (astronaut && astronaut.energy >= energyNeeded) {
 
+                    astronaut.energy -= value;
+                    console.log(`${name}has successfully explored a new area and now has ${astronaut.energy} energy`);
+
+                } else if (astronaut) {
+                    console.log(`${name} does not have enough energy to explore!`);
+
+                }
+
+                break;
+            case 'Refuel':
+                const amount = Number(value);
+                const astronautToRefuel = astronauts.find(astronaut => astronaut.name === name);
+                if (astronautToRefuel) {
+                    const energyRecovered = Math.min(amount, 200 - astronautToRefuel.energy);
+                    astronautToRefuel.energy += energyRecovered;
+
+                    console.log(`${name} refueled their energy by ${energyRecovered}!`);
+
+                    // astronautToRefuel.energy += amount;
+                    // if (astronautToRefuel.energy > 200) {
+                    //     astronautToRefuel.energy = 200;
+                    // }
+                }
+
+                break;
+            case 'Breathe':
+                const amountOxygen = Number(value);
+                const astronautToBreath = astronauts.find(astronaut => astronaut.name === name);
+                if (astronautToBreath) {
+                    const oxygenRecovered = Math.min(amountOxygen, 100 - astronautToBreath.oxygen);
+                    astronautToBreath.oxygen += oxygenRecovered;
+
+                    console.log(`${name} took a breath and recoverd ${oxygenRecovered} oxygen!`);
+
+                }
+
+                break;
+            default:
+                break;
+        }
+
+    }
+astronauts.forEach(astronaut => {
+    console.log(`Astronaut: ${astronaut.name}, Oxygen: ${astronaut.oxygen}, Energy: ${astronaut.energy}`);
+})
 
 
 }
